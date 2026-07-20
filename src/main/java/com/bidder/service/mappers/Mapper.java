e15ce8a1c986dd1e9f14142b689716eb9f826ffd
ammad-qureshi-dev <ammadq3@gmail.com>
2026-07-20 14:23:31 -0400

    chore: removing notification-related files not part of this service

--- src/main/java/com/bidder/service/mappers/Mapper.java
+++
@@ -1,9 +0,0 @@
-/* (C) 2026 
-bidder.app */
-package com.bidder.service.mappers;
-
-public interface Mapper<T> {
-
-	Object entityToResponse(T entity);
-	T responseToEntity(Object entity);
-}
--- src/main/java/com/bidder/service/models/ContactMethod.java
+++
@@ -1,7 +0,0 @@
-/* (C) 2026 
-bidder.app */
-package com.bidder.service.models;
-
-public enum ContactMethod {
-	EMAIL, APP, MOBILE
-}
